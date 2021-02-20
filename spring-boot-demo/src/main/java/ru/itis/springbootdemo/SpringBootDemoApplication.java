b6df96747a6a87616e094afc416f7f8d3e2ff071
yangirova01 <55754579+yangirova01@users.noreply.github.com>
2021-02-20 14:30:56 +0300

    first hw

---
+++ spring-boot-demo/src/main/java/ru/itis/springbootdemo/SpringBootDemoApplication.java
@@ -0,0 +1,16 @@
+package ru.itis.springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoApplication.class, args);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("хеллоу");
+    }
+}
